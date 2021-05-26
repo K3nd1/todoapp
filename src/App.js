3fe9ec015eaff3978a2f1ae6ab63858273e22ce1
Kendi Gjepali <42900883+K3nd1@users.noreply.github.com>
2021-05-27 00:32:45 +0200

    Update App.js
    Changed

--- src/App.js
+++ src/App.js
@@ -1,63 +1,57 @@
-import React,{ useEffect,useState} from 'react';
-import './App.css';
-import TodoForm from './components/TodoForm'
-import TodoList from './components/TodoList'
-import Typography from '@material-ui/core/Typography'
-const LOCAL_STORAGE_KEY = "todoapp-todos"
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import Typography from "@material-ui/core/Typography";
+const LOCAL_STORAGE_KEY = "todoapp-todos";
 
 function App() {
-  const [todos,setTodos] = useState([])
-  useEffect(() =>{
-   const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
 
-    if(storageTodos){
-      setTodos(storageTodos)
+    if (storageTodos) {
+      setTodos(storageTodos);
     }
+  }, []);
 
-  }, [])
-  
-  
-  useEffect(() =>{
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }, [todos])
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
-  function addTodo(todo){
-    setTodos([todo, ...todos])
+  function addTodo(todo) {
+    setTodos([todo, ...todos]);
   }
 
-
-  function toggleComplete(id){
+  function toggleComplete(id) {
     setTodos(
-      todos.map(todo =>{
-        if(todo.id === id){
-          return{
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
             ...todo,
-            completed: !todo.completed
-          }
+            completed: !todo.completed,
+          };
         }
-        
-        return todo
+
+        return todo;
       })
-    )
+    );
   }
 
-    function removeTodo(id){
-      setTodos(todos.filter(todo => todo.id !== id))
-    }
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
   return (
     <div className="App">
-     
-      <Typography
-      variant="h1"
-      style={{padding: 16}}
-      >React Todo</Typography>
-      <TodoForm addTodo={addTodo}  />
-      <TodoList 
-      todos={todos}  
-      toggleComplete={toggleComplete} 
-      removeTodo={removeTodo}
+      <Typography variant="h1" style={{ padding: 16 }}>
+        React Todo
+      </Typography>
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        toggleComplete={toggleComplete}
+        removeTodo={removeTodo}
       />
- 
     </div>
   );
 }
